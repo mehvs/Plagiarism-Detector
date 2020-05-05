fb704af1035265895a15c3f29a8d871b4f126acf
Ptr <petarman96@abv.bg>
2020-05-05 18:02:48 +0300

    added a new method which counts the unique words in a text file

---
+++ src/Detector.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Detector {
+
+    public int countsUniqueWords(ArrayList<String> storedWords){
+        Set<String> set = new HashSet<>();
+        Set<String> common = new HashSet<>();
+        for (String i : storedWords) {
+            if(!set.add(i)) {
+                common.add(i);
+            }
+        }
+        return (set.size() - common.size());
+    }
+
+}
