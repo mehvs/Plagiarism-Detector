ab350018ccad2cc9c9555083df7fba09cb08b3ad
Ptr <petarman96@abv.bg>
2020-05-05 17:34:37 +0300

    first commit

---
+++ src/IDetector.java
@@ -0,0 +1,11 @@
+public interface IDetector {
+
+    int countsUniqueWords();
+    int countsDifferentWords();
+
+    double getsAverageLengthOfWords();
+    double getsTypeTokenRation();
+    double getsHapaxLegomenaRatio();
+    double getsAverageWordsInSentence();
+
+}
