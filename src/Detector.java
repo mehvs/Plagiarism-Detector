e0cd81e142ba32346accbdc30d7ab6ff833dd237
Ptr <petarman96@abv.bg>
2020-05-05 18:12:33 +0300

    added the four features as methods from the task assignment, created a main class

--- src/Detector.java
+++ src/Detector.java
@@ -22,4 +22,29 @@ public class Detector {
                 .count();
     }
 
+    //Gets the average length of words in a string (Feature 1)
+    public double getsAverageLengthOfWords(ArrayList<String> storedWords) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < storedWords.size(); i++) {
+            sb.append(storedWords.get(i));
+        }
+        return (double)sb.length() / (double)storedWords.size();
+    }
+
+    //Gets the TTR (Feature 2)
+    public double getsTypeTokenRatio(ArrayList<String> storedWords){
+        return (double)countsDifferentWords(storedWords)/(double)storedWords.size();
+    }
+
+    //Gets the HLR (Feature 3)
+    public double getsHapaxLegomenaRatio(ArrayList<String> storedWords){
+        return (double)countsUniqueWords(storedWords)/(double)storedWords.size();
+    }
+
+    //Gets the average words in sentences (Feature 4)
+    public double getsAverageWordsInSentence(ArrayList<String> storedWords, ArrayList<String> storedSentences){
+        return (double)storedWords.size()/(double)storedSentences.size();
+    }
+
 }
